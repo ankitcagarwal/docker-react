42d26498c199db7711402addc41166d3b9c84aca
ankitcagarwal <ankitcagarwal@gmail.com>
2020-10-10 23:47:46 -0400

    first commit

---
+++ build/precache-manifest.865fc160d8d62e27ef6912575a15a519.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b98263e7e67e72c759f80cd2e43f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e260f4c000b5e377650f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e260f4c000b5e377650f",
+    "url": "/static/js/main.d02b09fe.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
